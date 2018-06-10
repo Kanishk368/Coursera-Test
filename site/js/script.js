0b6d0ade4d57a09b865346d52bd0bfb0151a3825
Kanishk368 <kanishk368@gmail.com>
2018-06-10 19:48:27 +0530

    Started 4 assignment

--- site/js/script.js
+++ site/js/script.js
@@ -21,12 +21,17 @@
 // var twotable = makeMultiplier(2);
 // console.log(twotable(5));
 
-function Circle (radius){
-	console.log(this);
-}
-console.log(this);
-var mycircle = new Circle(10);
-console.log(this);
-console.log(mycircle);
+// function Circle (radius){
+// 	console.log(this);
+// }
+// console.log(this);
+// var mycircle = new Circle(10);
+// console.log(this);
+// console.log(mycircle);
+var x= function (){
+	console.log("hjgj");
+	return;
+};
+x();
 
 
