dfad11127fa49a7d7c0ea96a919119b15404d09e
unknown <Kanishk368>
2018-06-10 09:40:28 +0530

    module2 Asignment Complete

--- site/js/script.js
+++ site/js/script.js
@@ -1,5 +1,32 @@
-function multiply(x,y) {
-	return x*y;
+// var a = new Object();
+// a.name = "kanishk";
+// a.age = "20";
+// a.display = function (){
+// 	console.log(a.name+" is "+a.age+"years old");
+// }
+// a.like = new Object();
+// a.like.sports = "basketball";
+
+// console.log(a.toString());
+// console.log(a);
+// // a.display();
+
+// var makeMultiplier = function (multiplier){
+// 	var myfunc = function(x){
+// 		return multiplier*x;
+// 	}
+// 	return myfunc;
+// }
+
+// var twotable = makeMultiplier(2);
+// console.log(twotable(5));
+
+function Circle (radius){
+	console.log(this);
 }
-console.log(multiply(3,8));
-multiply.version = "v.1.0.0"
+console.log(this);
+var mycircle = new Circle(10);
+console.log(this);
+console.log(mycircle);
+
+
